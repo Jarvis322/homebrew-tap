75d98fe09f7ffe5e9159384e28def6f83b4e57d8
Yigit Can Polat <yigitcpolat@gmail.com>
2026-06-27 22:55:09 +0300

    update macwake to 1.15

--- Casks/macwake.rb
+++ Casks/macwake.rb
@@ -1,6 +1,6 @@
 cask "macwake" do
-  version "1.14"
-  sha256 "d5159286c65f2ea944a58b99e29c8712d8519b40038afdf71d650d277e8a2109"
+  version "1.15"
+  sha256 "2282880b70d68e2ee72bf56b6bf8baba046a37e4196fa7c7e13123d44f7766f7"
 
   url "https://github.com/Jarvis322/MacWake/releases/download/v#{version}/Wake-#{version}.dmg"
   name "MacWake"
