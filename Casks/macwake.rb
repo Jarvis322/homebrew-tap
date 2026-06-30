13ca60de5743a68c2aad3a0b826fe5ef139d41a7
Yigit Can Polat <yigitcpolat@gmail.com>
2026-06-30 21:27:19 +0300

    update macwake to 1.22

--- Casks/macwake.rb
+++ Casks/macwake.rb
@@ -1,6 +1,6 @@
 cask "macwake" do
-  version "1.21"
-  sha256 "57043f2c11c147be2560557c244b906a8ac7ad773dd12d82f24d00a881755309"
+  version "1.22"
+  sha256 "5acfad399fb0fc427fdfc69cc7b423439619231d7be82c4db60c1ab7a7800269"
 
   url "https://github.com/Jarvis322/MacWake/releases/download/v#{version}/Wake-#{version}.dmg"
   name "MacWake"
