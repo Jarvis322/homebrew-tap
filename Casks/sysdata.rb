f0e5a7bedaf8b99dffa3a892364398170cef08a9
Yigit <yigitcpolat@gmail.com>
2026-09-06 18:29:32 +0300

    sysdata 0.3.5

--- Casks/sysdata.rb
+++ Casks/sysdata.rb
@@ -1,8 +1,8 @@
 cask "sysdata" do
-  version "0.3.4"
-  sha256 "5a9d25e6ac237e749585b2ccc6a5eb27295b4b349c29b2602b4d473dd228d0cd"
+  version "0.3.5"
+  sha256 "e62b39bd81f08fabfab1cc0965489513a6550e5e4e8be3955f15a6685bdbfd26"
 
-  url "https://github.com/Jarvis322/macos-sysdata/releases/download/v#{version}/SysDataMenu-#{version}.zip"
+  url "https://github.com/Jarvis322/macos-sysdata/releases/download/v#{version}/SysDataMenu-#{version}.dmg"
   name "System Data"
   desc "Menu bar app that shows what is inside macOS System Data and deletes it item by item"
   homepage "https://github.com/Jarvis322/macos-sysdata"
