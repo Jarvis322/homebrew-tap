dd5ecda521fdc2c1740bb5d9c663fe8b1ef98688
Yigit <yigitcpolat@gmail.com>
2026-09-11 13:51:47 +0300

    sysdata 1.0.5

--- Casks/sysdata.rb
+++ Casks/sysdata.rb
@@ -1,15 +1,15 @@
 cask "sysdata" do
-  version "1.0.4"
-  sha256 "0ab56a9071f02434d46b224e66ee062f951e86f3a706fcf08c891438298025cb"
+  version "1.0.5"
+  sha256 "6bc96f60f5e4c934a42fd28de49f930949cf6aa8e7bfd476f065299c406735e0"
 
   url "https://github.com/Jarvis322/macos-sysdata/releases/download/v#{version}/SysDataMenu-#{version}.dmg"
-  name "System Data"
+  name "System Data Unpacked"
   desc "Menu bar app that shows what is inside macOS System Data and deletes it item by item"
   homepage "https://github.com/Jarvis322/macos-sysdata"
 
   depends_on macos: :sonoma
 
-  app "SysDataMenu.app"
+  app "System Data Unpacked.app"
 
   # The history file records the paths of everything scanned and deleted. It
   # is the one artefact worth being thorough about, and it was the one this
