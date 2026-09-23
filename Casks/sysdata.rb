b9a71e774752526a689bb69bac65b1d4022d75e6
Yigit <yigitcpolat@gmail.com>
2026-09-23 11:15:27 +0300

    sysdata 1.5.0

--- Casks/sysdata.rb
+++ Casks/sysdata.rb
@@ -1,6 +1,6 @@
 cask "sysdata" do
-  version "1.4.1"
-  sha256 "5cd5332a20302c3878b1b962fb422c86560befe446acc20ee859a6d0151e9176"
+  version "1.5.0"
+  sha256 "edd765b11f58bce7e348c1a6cb6457b7a7daf68d9573f6a0d4089b95ad447c61"
 
   url "https://github.com/Jarvis322/macos-sysdata/releases/download/v#{version}/SysDataMenu-#{version}.dmg"
   name "System Data Unpacked"
